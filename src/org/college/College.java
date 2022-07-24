053387aba155a7dfef83dcba8588f7a756f8a84b
Revanjr11 <revanjr1401@gmail.com>
2022-07-24 11:32:11 +0530

    new code

---
+++ src/org/college/College.java
@@ -0,0 +1,124 @@
+package org.college;
+
+public class College {
+	private void collegeName() {
+       
+		System.out.println(" COLLEGE NAME - ANNA UNIVERSITY.");
+
+	}
+	
+	private void collegeCode() {
+		
+		System.out.println(" COLLEGE CODE - 1100100.");
+		
+	}
+
+	
+	private void collegeRank() {
+		
+		System.out.println(" COLLEGE RANK - Anna University is ranked #1101 in Best Global Universities.");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		College C = new College();
+		Student S = new Student();
+		Hostel  H = new Hostel ();
+		Dept    D = new Dept   ();
+		
+		C.collegeName();
+		C.collegeCode();
+		C.collegeRank();
+		S.studentName();
+		S.studentDept();
+		S.studentId();
+		H.hostelName();
+		D.deptName();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/org/college/Student.java
@@ -0,0 +1,23 @@
+package org.college;
+
+public class Student {
+
+	
+	public void studentName() {
+		
+		System.out.println(" STUDENT NAME - DIVYADHARSHINI.");
+	}
+	
+	public void studentDept() {
+		
+		System.out.println(" STUDENT DEPT - Faculty of Technology");
+	}
+	
+	public void studentId() {
+		
+		System.out.println(" STUDENT ID - 10042001");
+	}
+	
+	
+	
+}
